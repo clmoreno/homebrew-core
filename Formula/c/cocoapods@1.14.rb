331bebbc25eabf897be9782907c3a1a1a1b25fcb
Carlos Moreno <cmoreno@protelecom.com.gt>
2024-02-23 20:18:31 -0600

    cocoapods 1.14.3 (new formula)

--- Formula/c/cocoapods@1.14.rb
+++ Formula/c/cocoapods@1.14.rb
@@ -1,4 +1,4 @@
-class CocoapodsAT14 < Formula
+class CocoapodsAT114 < Formula
   desc "Dependency manager for Cocoa projects"
   homepage "https://cocoapods.org/"
   url "https://github.com/CocoaPods/CocoaPods/archive/refs/tags/1.14.3.tar.gz"
