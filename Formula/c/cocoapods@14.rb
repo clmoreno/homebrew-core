42ab47a6271e8d66493693a0ee30a3a4710a024a
Carlos Moreno <cmoreno@protelecom.com.gt>
2024-02-23 19:49:18 -0600

    cocoapods 1.14.3 (new formula)

---
+++ Formula/c/cocoapods@14.rb
@@ -0,0 +1,26 @@
+class CocoapodsAT14 < Formula
+  desc "Dependency manager for Cocoa projects"
+  homepage "https://cocoapods.org/"
+  url "https://github.com/CocoaPods/CocoaPods/archive/refs/tags/1.14.3.tar.gz"
+  sha256 "de05766e5771e0cef7af89f73b0e42a1f1c52a76ce1288592cd9511bcd688a9e"
+  license "MIT"
+
+  keg_only :versioned_formula
+
+  depends_on "pkg-config" => :build
+  depends_on "ruby"
+  uses_from_macos "libffi", since: :catalina
+
+  def install
+    ENV["GEM_HOME"] = libexec
+    system "gem", "build", "cocoapods.gemspec"
+    system "gem", "install", "cocoapods-#{version}.gem"
+    # Other executables don't work currently.
+    bin.install libexec/"bin/pod", libexec/"bin/xcodeproj"
+    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+  end
+
+  test do
+    system "#{bin}/pod", "list"
+  end
+end
